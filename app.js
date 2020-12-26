bdf57e8b7e31ad5370aaf131af1b2020dcbed01d
Nikola Novak <nikola.novak.hr@gmail.com>
2020-12-26 17:39:59 +0100

    Added enums

--- app.js
+++ app.js
@@ -10,14 +10,23 @@ var number2 = 2.8;
 var printResult = true;
 var resultPhrase = "The result is: ";
 add(number1, number2, printResult, resultPhrase);
+var Country;
+(function (Country) {
+    Country[Country["CROATIA"] = 5] = "CROATIA";
+    Country[Country["GERMANY"] = 100] = "GERMANY";
+    Country[Country["NORWAY"] = 101] = "NORWAY";
+    Country["SWEDEN"] = "SWE";
+})(Country || (Country = {}));
 var person = {
     name: "Nikola",
     age: 30,
     hobbies: ["Sports", "Cooking"],
-    role: [2, "Author"]
+    role: [2, "Author"],
+    country: Country.CROATIA
 };
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby);
 }
+console.log(person.country); // will log 5
--- app.ts
+++ app.ts
@@ -12,17 +12,27 @@ let resultPhrase = "The result is: ";
 
 add(number1, number2, printResult, resultPhrase);
 
+enum Country {
+  CROATIA = 5, // changing the starting index, default is 0
+  GERMANY = 100, // this would normally be 6 (since 5 is the starting index), but can be set to anything
+  NORWAY, // this will now be 101
+  SWEDEN = "SWE", // strings can also be used
+}
+
 const person: {
   name: string;
   age: number;
   hobbies: string[];
   role: [number, string]; // tuple
+  country: Country; // enum
 } = {
   name: "Nikola",
   age: 30,
   hobbies: ["Sports", "Cooking"],
   role: [2, "Author"],
+  country: Country.CROATIA,
 };
+
 interface TSOnlyComment_Tuple {
   // dummy interface for writing comments only in TS (won't be compiled to JS)
   // person.role[1] = 10; -> this will not work because the second element has to be a string
@@ -35,3 +45,5 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
   console.log(hobby);
 }
+
+console.log(person.country); // will log 5
