4cf1a7de50f3242c605bd18f48c24b6d8cc1c317
Nikola Novak <nikola.novak.hr@gmail.com>
2020-12-28 22:40:48 +0100

    Added an unknown type

---
+++ app.ts
@@ -0,0 +1,7 @@
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Nikola";
+// if userInput was of type 'any' this check would not be needed, but for the 'unknown' type it is needed
+if (typeof userInput === "string") userName = userInput;
