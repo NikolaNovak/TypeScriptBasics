beded9d690761ebf7cad14cd482c99d10e4e4f1a
Nikola Novak <nikola.novak.hr@gmail.com>
2020-12-28 22:47:28 +0100

    Added a never type

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ userInput = "Nikola";
 // if userInput was of type 'any' this check would not be needed, but for the 'unknown' type it is needed
 if (typeof userInput === "string")
     userName = userInput;
+// the never type means that the function never returns anything because it stops the script execution
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+var result = generateError("An error occured!", 500);
+console.log("The result:" + result); // this will not be executed because an error is thrown on the previous line
