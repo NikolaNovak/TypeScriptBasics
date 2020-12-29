8addaa6a8330d5274dc88c33bd79538bbb32c1f4
Nikola Novak <nikola.novak.hr@gmail.com>
2020-12-29 17:56:42 +0100

    Moved code from app.ts to unknown&never.ts and compiled it

---
+++ unknown&never.js
@@ -0,0 +1,14 @@
+"use strict";
+let userInput;
+let userName;
+userInput = 5;
+userInput = "Nikola";
+// if userInput was of type 'any' this check would not be needed, but for the 'unknown' type it is needed
+if (typeof userInput === "string")
+    userName = userInput;
+// the never type means that the function never returns anything because it stops the script execution
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+const result = generateError("An error occured!", 500);
+console.log("The result:" + result); // this will not be executed because an error is thrown on the previous line
